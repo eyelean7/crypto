cf5a90581472585da19104c06d81393e07bc3b63
Anatoliy Klots and Ilene Gorski <ilene@>
2017-03-30 16:36:55 -0700

    Add loop to iterate through each string segment and push first character to new array

--- js/scripts.js
+++ js/scripts.js
@@ -8,14 +8,25 @@ function encryptor(inputSentence) {
   sentenceLength = sentenceArray.length;
   var sentenceRoot = Math.sqrt(sentenceLength);
   var sentenceRootProduct = Math.ceil(sentenceRoot);
-
+  //break string based on square root of input
+  var matrixArray = [];
   for (var i=0; i<sentenceLength; i+=sentenceRootProduct) {
-  alert(sentence.substr(i, sentenceRootProduct))
+    matrixArray.push(sentence.substr(i, sentenceRootProduct));
+
+  }//for loop creates split strings
+  var outputArray = [];
+  for (var i=0; i<matrixArray.length; i++) {
+     outputArray.push(matrixArray[i][0]);
+    // for (var i=0; i<=sentenceRootProduct; i++) {
+    //   outputArray.push(matrixArray[i][i]);
+    // }
+
   }
 
 
+  console.log(outputArray);
 
-}
+}//encryptor
 
 //UI
 
